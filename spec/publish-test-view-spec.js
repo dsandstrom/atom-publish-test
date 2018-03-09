51bcac793635f17f2b4aef23016ab8095a50f481
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2018-03-08 16:43:34 -0800

    Generate package

---
+++ spec/publish-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PublishTestView from '../lib/publish-test-view';
+
+describe('PublishTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
